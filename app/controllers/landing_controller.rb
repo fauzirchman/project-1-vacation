2512c8c0d0d1aba9654a0c295b6912b92459fa7c
Fauzi Rachman <fauziiirachman@gmail.com>
2018-07-07 15:22:58 +0700

    Initial commit

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,13 @@
+class LandingController < ApplicationController
+
+	def index; end
+
+  def describe; end
+
+  def showcase; end
+
+  def neighbors; end
+
+  def help; end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'landing#index'
+
+  get 'describe', to: 'landing#describe', as: :describe
+  get 'showcase', to: 'landing#showcase', as: :showcase
+  get 'neighbors', to: 'landing#neighbors', as: :neighbors
+  get 'help', to: 'landing#help', as: :help
+end
